bd24f45a25e3b4123f180e6e3e3d38d80bc70080
estel <peraltajohnestel@gmail.com>
2022-08-04 03:18:09 +0800

    implemented user sending of id for verification

---
+++ api/checkVerifiedUser.php
@@ -0,0 +1,23 @@
+<?php
+    include 'connection.php';
+    if($conn->connect_error){
+        die($conn->connect_error);
+    }
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = $data['userID'];
+    $sql = "SELECT userID, verified FROM userverification
+            WHERE userverification.verified = 'TRUE'
+            AND userverification.userID =".$id.";";
+    $result = $conn->query($sql);
+    $d = [];
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+           $d["userID"]=$row['userID'];
+           $d["verified"]=$row['verified'];
+        }
+        echo json_encode($d);
+    }else{
+        $d["userID"]=0;
+        echo json_encode($d);   
+    }
+?>
---
+++ api/uploadUserVerification.php
@@ -0,0 +1,19 @@
+<?php
+    include 'connection.php';
+    if($conn->connect_error){
+        die($conn->connect_error);
+    }
+    $content = trim(file_get_contents("php://input"));
+    $userData = json_decode($content, true);
+    $sql = "INSERT INTO userverification
+            (userID, id1, id2, verified)
+            VALUES 
+            (
+                '{$userData['userID']}',
+                '{$userData['id1']}',
+                '{$userData['id2']}',
+                'FALSE'
+            );";
+    $result = $conn->query($sql);
+    echo json_encode($content);
+?>
