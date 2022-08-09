c97b666fa393ffc823ac7e5fd97d2d6bba0005cb
John Estel Peralta <=>
2022-08-09 23:03:38 +0800

    implemented posting order for services

---
+++ api/getOrderPosts.php
@@ -0,0 +1,36 @@
+<?php
+    include 'connection.php';
+    if($conn->connect_error){
+        die($conn->connect_error);
+    }
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = $data['userID'];
+    $sql = "SELECT
+            serviceorder.serviceOrderID,
+            servicecategory.categoryName,
+            service.serviceName,
+            serviceorder.description,
+            serviceorder.serviceOrderDateTime
+            FROM serviceorder
+            JOIN service ON service.serviceID = serviceorder.serviceID
+            JOIN servicecategory ON servicecategory.serviceCategoryID = service.serviceCategoryID
+            WHERE serviceorder.userID =".$id."
+            ORDER BY serviceorder.serviceOrderDateTime DESC;";
+    $result = $conn->query($sql);
+    $d = [];
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            array_push($d, array(
+                "serviceOrderID"=>$row['serviceOrderID'],
+                "categoryName"=>$row['categoryName'],
+                "serviceName"=>$row['serviceName'],
+                "description"=>$row['description'],
+                "serviceOrderDateTime"=>$row['serviceOrderDateTime']
+            ));
+        }
+        echo json_encode($d);
+    }else{
+        $d["posts"]=0;
+        echo json_encode($d);   
+    }
+?>
