8c56aa7d76c39e5fa89e2911011ff2db0a7e4aa6
John Estel Peralta <=>
2022-08-14 01:57:12 +0800

    editing business page (title, header photo, description)

---
+++ api/getBusinessPageInfo.php
@@ -0,0 +1,28 @@
+<?php
+    include 'connection.php';
+    if($conn->connect_error){
+        die($conn->connect_error);
+    }
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = $data['postID'];
+    $sql = "SELECT *
+            FROM premiumpost
+            WHERE pID =". $id .";";
+    $result = $conn->query($sql);
+    $d = [];
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            array_push($d, array(
+                "title"=>$row['title'],
+                "description"=>$row['description'],
+                "featuredPhoto"=>$row['featuredPhoto'],
+                "postDate"=>$row['postDate'],
+                "postDuration"=>$row['postDuration']
+            ));
+        }
+        echo json_encode($d);
+    }else{
+        $d["posts"]=0;
+        echo json_encode($d);   
+    }
+?>
--- js/createBusinessPageBehavior.js
+++ js/createBusinessPageBehavior.js
@@ -2,10 +2,41 @@ $("document").ready(()=>{
 
     let businessPageID = sessionStorage.getItem("businessPage");
     let slideshowImages = ["../images/icons8-picture-500.png"];
+    let pageInfo;
     //track the index of current image from slideshow image array
     let currentImg = 0;
 
-    console.log(businessPageID);
+    //load data
+    let getPostdata = ()=>{
+        fetch('../api/getBusinessPageInfo.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                postID: sessionStorage.getItem("businessPage")
+            })
+        })
+        .then(res=>{return res.json()})
+        .then(data=>{
+            console.log(data);
+            pageInfo = data;
+            $("#duration").text("duration: " + data[0].postDuration);
+            $("#businessName").val(data[0].title);
+            if(data[0].featuredPhoto == undefined){
+                console.log("wala");
+                $("#featuredPhoto").attr("src", "../images/icons8-picture-500.png");
+            }else{
+                $("#featuredPhoto").attr("src", data[0].featuredPhoto);
+            }
+            if(data[0].description != undefined){
+                $("#businessDetails").val(data[0].description);
+            }
+        })
+        .catch(error=>console.log("error on retrieval of this user posts"));
+    };
+
+    getPostdata();
 
     $("#businessName").keyup(()=>{
         console.log( $("#businessName").prop('scrollHeight'));
@@ -24,9 +55,6 @@ $("document").ready(()=>{
     $("#businessDetails").keyup();
 
     //slideshow behavior
-
-    
-
     let setImage = (img)=>{
         $(".slideshowImage").attr("src", slideshowImages[0]);
     };
@@ -50,4 +78,107 @@ $("document").ready(()=>{
         }
         $(".slideshowImage").attr("src", slideshowImages[currentImg]);
     });
+    
+    //edit title
+    $("#businessName").keyup(()=>{
+        $("#editBusinessName").attr("src", "../images/check.png");
+    });
+
+    $("#editBusinessName").click(()=>{
+        let name = $("#businessName").val();
+        name = name.replace("'", "\\'");
+        if($("#businessName").val() == pageInfo[0].title){
+            $("#businessName").css({border: "1px solid gold"});
+            setTimeout(()=>{
+                $("#businessName").css({border: "1px solid grey"});
+            }, 1000);
+        }else{
+            fetch('../api/updateBusinessTitle.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: sessionStorage.getItem("businessPage"),
+                    title: name
+                })
+            })
+            .then(res=>{return res.json()})
+            .then(data=>{
+                $("#businessName").css({border: "1px solid gold"});
+                $("#editBusinessName").attr("src", "../images/edit.png");
+                setTimeout(()=>{
+                    $("#businessName").css({border: "1px solid grey"});
+                }, 1000);
+            })
+            .catch(error=>console.log("error on retrieval of this user posts"));
+        }
+    });
+
+    //edit description
+    $("#businessDetails").keyup(()=>{
+        $("#editBusinessDetails").attr("src", "../images/check.png");
+    });
+
+    $("#editBusinessDetails").click(()=>{
+        let details = $("#businessDetails").val();
+        details = details.replace("'", "\\'");
+        if($("#businessDetails").val() == pageInfo[0].details){
+            $("#businessDetails").css({border: "1px solid gold"});
+            setTimeout(()=>{
+                $("#businessDetails").css({border: "1px solid grey"});
+            }, 1000);
+        }else{
+            fetch('../api/updateBusinessDescription.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: sessionStorage.getItem("businessPage"),
+                    description: details
+                })
+            })
+            .then(res=>{return res.json()})
+            .then(data=>{
+                $("#businessDetails").css({border: "1px solid gold"});
+                $("#editBusinessDetails").attr("src", "../images/edit.png");
+                setTimeout(()=>{
+                    $("#businessDetails").css({border: "1px solid grey"});
+                }, 1000);
+            })
+            .catch(error=>console.log("error on retrieval of this user posts"));
+        }
+    });
+
+    //change header photo
+    function previewProfileImage(uploader) {   
+        //ensure a file was selected 
+        if (uploader.files && uploader.files[0]) {
+            var imageFile = uploader.files[0];
+            var reader = new FileReader();    
+            reader.onload = function (e) {
+                //set the image data as source
+                $('#featuredPhoto').attr('src', e.target.result);
+                //save photo to database
+                fetch('../api/updateBusinessHeaderPhoto.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        id: sessionStorage.getItem("businessPage"),
+                        photo: $('#featuredPhoto').attr('src')
+                    })
+                })
+                .catch(error=>console.log("error on retrieval of this user posts"));
+            }    
+            reader.readAsDataURL( imageFile );
+        }
+    }
+    
+    $("#uploadFeaturedPhoto").change(function(){
+        previewProfileImage(this);
+    });
+
 });
